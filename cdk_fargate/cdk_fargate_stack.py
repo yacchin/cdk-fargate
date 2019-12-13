8342621e7750ac6f3adf63448195019c2925e687
YasuakiMatsuda <yasuaki.matsuda@iridge.jp>
2019-12-13 10:06:06 +0900

    Add the task-definition.

--- cdk_fargate/cdk_fargate_stack.py
+++ cdk_fargate/cdk_fargate_stack.py
@@ -1,38 +1,44 @@
+import os
+from dotenv import load_dotenv
+
 from aws_cdk import (
     aws_ec2 as ec2,
     aws_ecs as ecs,
+    aws_iam as iam,
     aws_ecs_patterns as ecs_patterns,
     core,
 )
 
+load_dotenv()
+
+ROLE_ARN = os.environ["ROLE_ARN"]
+ECR_REGISOTRY = os.environ["ECR_REGISOTRY"]
+
 
 class CdkFargateStack(core.Stack):
-
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # The code that defines your stack goes here
-        # Create VPC and Fargate Cluster
-        # NOTE: Limit AZs to avoid reaching resource quotas
-        vpc = ec2.Vpc(
-            self, "CdkFargateVpc",
-            max_azs=2
-        )
+        vpc = ec2.Vpc(self, "CdkFargateVpc", max_azs=2)
 
-        cluster = ecs.Cluster(
-            self, 'Ec2Cluster',
-            vpc=vpc
+        cluster = ecs.Cluster(self, "Ec2Cluster", vpc=vpc)
+
+        role = iam.Role.from_role_arn(self, "Role", ROLE_ARN)
+        image = ecs.ContainerImage.from_registry(ECR_REGISOTRY)
+        task_definition = ecs.FargateTaskDefinition(
+            scope=self, id="TaskDefinition", execution_role=role, task_role=role
         )
+        port_mapping = ecs.PortMapping(container_port=80, host_port=80)
+        container = task_definition.add_container(
+            id="Container", image=image
+        ).add_port_mappings(port_mapping)
 
         fargate_service = ecs_patterns.ApplicationLoadBalancedFargateService(
-            self, "FargateService",
-            cluster=cluster,
-            task_image_options={
-                'image': ecs.ContainerImage.from_registry("nginx")
-            }
+            self, "FargateService", cluster=cluster, task_definition=task_definition
         )
 
         core.CfnOutput(
-            self, "LoadBalancerDNS",
-            value=fargate_service.load_balancer.load_balancer_dns_name
+            self,
+            "LoadBalancerDNS",
+            value=fargate_service.load_balancer.load_balancer_dns_name,
         )
